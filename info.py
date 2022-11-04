13520191c78ba927fd462cc229e9ea9fcdcee146
soham patkar <sohampatkar610@gmail.com>
2022-11-04 20:30:27 +0530

    new file created

---
+++ info.py
@@ -0,0 +1,27 @@
+#why python is dynamic language?
+#readability,easy to understand,no need to define datatype,
+
+#data types:-
+# integer(1,2,3,4),
+# float(1.5,1.4,2.5,3.6),
+# boolean (yes0/no1),
+# sting('soham','patkar'),('= single line,''=single line,'''=paragraph)
+
+#variable: to store any type of data. 
+#rules: 
+# #variable cant start by any integer,
+#it starts by 'alphabates' or '_'.
+#they are case sensitive.
+
+# '#' is used to define comments.they are easy to read and define data to user. & it is unexecutable.
+
+#data structures:
+#list=[]
+#tupple=()
+#set={}
+#dictionary={}
+
+#simple code
+#(print hello)
+print('hello!')
+
---
+++ test1.py
@@ -0,0 +1,13 @@
+#assi 1 replace
+tupple1=(1,2,3,4,8)
+lst=list(tupple1)
+print(lst)
+lst[4]=5
+print(lst)
+
+#assin 2 extend
+a=[1,2,3,4,5]
+b=[6,7,8,9,0]
+a.extend(b)
+print(a)
+
